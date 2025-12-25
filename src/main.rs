8d74ab424b73593a1b63e3ba2842f00f27a98630
heyitscarl-dev <about@heyitscarl.dev>
2025-12-25 01:03:07 +0100

    feat: implement simple http monitoring

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use tracing::info;
 
-use crate::{config::load_config, error::Fallible};
+use crate::{config::{Host, load_config}, error::Fallible};
 
 pub mod error;
 pub mod config;
@@ -12,5 +12,15 @@ fn main() -> Fallible {
     let count = config.hosts.len();
     info!("Found {} host{}.", count, if count == 1 { "" } else { "s" });
 
+    for host in config.hosts {
+        let ok = monitor(&host);
+        info!("Host '{}' is {}.", host.label, if ok { "up" } else { "down" })
+    }
+
     Ok(())
+}
+
+fn monitor(host: &Host) -> bool {
+    reqwest::blocking::get(&host.on)
+        .is_ok()
 }
